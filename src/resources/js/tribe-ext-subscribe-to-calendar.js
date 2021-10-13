8ca4466e095ae84216c35a331e89dbd53729079d
Gennady Kovshenin <gennady@kovshenin.com>
2021-10-13 20:33:30 +0300

    Fix dropdown on view change

--- src/resources/js/tribe-ext-subscribe-to-calendar.js
+++ src/resources/js/tribe-ext-subscribe-to-calendar.js
@@ -1,4 +1,4 @@
-jQuery( '.subscribe-to-calendar-dropdown-selector-button' ).on( 'click' , function() {
+jQuery( 'body' ).on( 'click' , '.subscribe-to-calendar-dropdown-selector-button',  function() {
 	jQuery( this ).toggleClass( 'subscribe-to-calendar-dropdown-selector-button-active' );
 	jQuery( this ).next().toggle();
 } );
